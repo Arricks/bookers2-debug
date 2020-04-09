daaca52c4ac5cc657d834b2c9fdc5ed004ab017f
Arricks <kabata.yuya@gmail.com>
2020-04-09 14:20:20 +0900

    finish debug

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+	root :to => 'home#top'
+	get 'home/about', to: 'home#about'
+	devise_for :users
+
+	resources :users,only: [:show, :index, :edit, :update]
+	resources :books
+
+end
